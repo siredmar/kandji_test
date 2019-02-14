eb9f4e5fb9e06624c596f5d23db796ff319007fa
Joel Hermanns <joel.hermanns@gmail.com>
2019-02-14 17:57:04 +0100

    feat(ouput): add wide output for pods

--- pkg/printer/console/pod_output.go
+++ pkg/printer/console/pod_output.go
@@ -13,9 +13,45 @@ type PodConsoleOutput struct {
 	Age      string `header:"Age"`
 }
 
+type PodConsoleOutputWide struct {
+	ID         string `header:"ID"`
+	DeviceID   string `header:"Device ID"`
+	Age        string `header:"Age"`
+	Containers string `header:"Containers"`
+	Image      string `header:"Images"`
+}
+
 func (o PodConsoleOutput) Map(p api.Pod) PodConsoleOutput {
 	o.ID = p.Metadata.ID
 	o.DeviceID = p.Spec.DeviceID
+	if p.Status.StartTime != nil {
+		startTime := p.Status.StartTime
+
+		if time.Since(startTime.Time).Seconds() < 60 {
+			o.Age = fmt.Sprintf("%.0fs", time.Since(startTime.Time).Seconds())
+		} else if time.Since(startTime.Time).Minutes() < 60 {
+			o.Age = fmt.Sprintf("%.0fm", time.Since(startTime.Time).Minutes())
+		} else if time.Since(startTime.Time).Hours() < 24 {
+			o.Age = fmt.Sprintf("%.0fh", time.Since(startTime.Time).Hours())
+		} else {
+			o.Age = fmt.Sprintf("%.0fd", time.Since(startTime.Time).Hours()/24)
+		}
+	}
+	return o
+}
+
+func (o PodConsoleOutputWide) Map(p api.Pod) PodConsoleOutputWide {
+	o.ID = p.Metadata.ID
+	o.DeviceID = p.Spec.DeviceID
+	var images string
+	var containers string
+	for _, container := range p.Spec.Config.Containers {
+		images += fmt.Sprintf("%s\n", container.Image)
+		containers += fmt.Sprintf("%s\n", container.Name)
+	}
+	o.Image = images
+	o.Containers = containers
+
 	if p.Status.StartTime != nil {
 		startTime := p.Status.StartTime
 
