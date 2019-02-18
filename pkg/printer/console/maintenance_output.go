11f1785976bfe1a074727e901746417e83e02f8c
ecktom <t.eck@gridx.de>
2019-02-18 16:51:15 +0100

    feat(maintenance): Adding wide output

--- pkg/printer/console/maintenance_output.go
+++ pkg/printer/console/maintenance_output.go
@@ -1,8 +1,10 @@
 package printer
 
 import (
-	api "github.com/grid-x/gxctl/pkg/api"
+	"fmt"
 	"strconv"
+
+	api "github.com/grid-x/gxctl/pkg/api"
 )
 
 type MaintenanceConsoleOutput struct {
@@ -12,11 +14,37 @@ type MaintenanceConsoleOutput struct {
 	Running    string `header:"Running"`
 }
 
+type MaintenanceConsoleOutputWide struct {
+	ID         string `header:"ID"`
+	Successful string `header:"Successful"`
+	Failed     string `header:"Failed"`
+	Running    string `header:"Running"`
+	Selector   string `header:"Selector"`
+}
+
 func (o MaintenanceConsoleOutput) Map(m api.MaintenanceTask) MaintenanceConsoleOutput {
 	o.ID = m.Metadata.ID
 	o.Successful = strconv.Itoa(m.Status.Successful)
 	o.Failed = strconv.Itoa(m.Status.Failed)
 	o.Running = strconv.Itoa(m.Status.Running)
+
+	return o
+}
+
+func (o MaintenanceConsoleOutputWide) Map(m api.MaintenanceTask) MaintenanceConsoleOutputWide {
+	o.ID = m.Metadata.ID
+	o.Successful = strconv.Itoa(m.Status.Successful)
+	o.Failed = strconv.Itoa(m.Status.Failed)
+	o.Running = strconv.Itoa(m.Status.Running)
+
+	if m.Spec.Selector.MatchByLabels != nil {
+		var s string
+		for key, value := range m.Spec.Selector.MatchByLabels {
+			s += fmt.Sprintf("%s:%s\n", key, value)
+		}
+
+		o.Selector = s[:len(s)-1]
+	}
 
 	return o
 }
--- pkg/printer/console/maintenances_output.go
+++ pkg/printer/console/maintenances_output.go
@@ -7,6 +7,7 @@ import (
 )
 
 type MaintenancesConsoleOutput []MaintenanceConsoleOutput
+type MaintenancesConsoleOutputWide []MaintenanceConsoleOutputWide
 
 func (mo MaintenancesConsoleOutput) Map(m api.MaintenanceTasks) MaintenancesConsoleOutput {
 	var out MaintenancesConsoleOutput
@@ -17,7 +18,24 @@ func (mo MaintenancesConsoleOutput) Map(m api.MaintenanceTasks) MaintenancesCons
 	return out
 }
 
+func (mo MaintenancesConsoleOutputWide) Map(m api.MaintenanceTasks) MaintenancesConsoleOutputWide {
+	var out MaintenancesConsoleOutputWide
+	for _, e := range m.MaintenanceTasks {
+		out = append(out, MaintenanceConsoleOutputWide{}.Map(e))
+	}
+
+	return out
+}
+
 func (mo MaintenancesConsoleOutput) Sort() MaintenancesConsoleOutput {
+	sort.Slice(mo, func(i, j int) bool {
+		return mo[j].ID > mo[i].ID
+	})
+
+	return mo
+}
+
+func (mo MaintenancesConsoleOutputWide) Sort() MaintenancesConsoleOutputWide {
 	sort.Slice(mo, func(i, j int) bool {
 		return mo[j].ID > mo[i].ID
 	})
