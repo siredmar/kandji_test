080441019b22e84351e93cc2116a59760739f69c
ecktom <t.eck@gridx.de>
2019-07-01 10:17:56 +0200

    feat(printer): Enhance device output

--- pkg/printer/console/device_output.go
+++ pkg/printer/console/device_output.go
@@ -9,6 +9,7 @@ type DeviceConsoleOutput struct {
 	Serialnumber      string `header:"Serialnumber"`
 	MaintenanceWindow string `header:"Maintenance Window"`
 	MACAddress        string `header:"MAC address"`
+	LastHeartbeat     string `header:"Last heartbeat"`
 }
 
 type DeviceConsoleOutputWide struct {
@@ -17,6 +18,9 @@ type DeviceConsoleOutputWide struct {
 	MaintenanceWindow string `header:"Maintenance Window"`
 	MACAddress        string `header:"MAC address"`
 	LastHeartbeat     string `header:"Last heartbeat"`
+	FirstSeen         string `header:"First seen"`
+	SuperviorVersion  string `header:"Supervisor"`
+	OSVersion         string `header:"OS"`
 	Labels            string `header:"Labels"`
 }
 
@@ -28,6 +32,9 @@ func (o DeviceConsoleOutput) Map(d api.Device) DeviceConsoleOutput {
 	}
 	if d.Spec.MACAddress != nil {
 		o.MACAddress = *d.Spec.MACAddress
+	}
+	if d.Status.LastHeartbeat != nil {
+		o.LastHeartbeat = d.Status.LastHeartbeat.Format("02.01.2006 15:04:05")
 	}
 	return o
 }
@@ -42,10 +49,21 @@ func (o DeviceConsoleOutputWide) Map(d api.Device) DeviceConsoleOutputWide {
 		o.MACAddress = *d.Spec.MACAddress
 	}
 	if d.Status.LastHeartbeat != nil {
-		o.LastHeartbeat = d.Status.LastHeartbeat.String()
+		o.LastHeartbeat = d.Status.LastHeartbeat.Format("02.01.2006 15:04:05")
+	}
+	if d.Status.FirstSeen != nil {
+		o.FirstSeen = d.Status.FirstSeen.Format("02.01.2006 15:04:05")
 	}
 	if d.Metadata.Labels != nil {
 		o.Labels = SortedString(d.Metadata.Labels)
+	}
+	if d.Status.Info != nil {
+		if d.Status.Info.SupervisorVersion != nil {
+			o.SuperviorVersion = *d.Status.Info.SupervisorVersion
+		}
+		if d.Status.Info.OSVersion != nil {
+			o.OSVersion = *d.Status.Info.OSVersion
+		}
 	}
 	return o
 }
