31ac1da57013a9549343dbfb34a694d6fbc28031
tobjaw <tobjaw@gmail.com>
2020-04-29 01:37:48 +0200

    feat(lint): add rule DeploymentVolumesExist

---
+++ internal/lint/rule/deploymentVolumesExist.go
@@ -0,0 +1,66 @@
+package rule
+
+import (
+	"github.com/grid-x/gxctl/internal/lint/context"
+	"github.com/grid-x/gxctl/internal/lint/result"
+	"github.com/grid-x/gxctl/pkg/api"
+	"github.com/grid-x/gxctl/pkg/errors"
+)
+
+// DeploymentVolumesExist passes iff all volumes referenced by volumeMounts exist.
+type DeploymentVolumesExist struct{}
+
+// ID returns the ID of this rule
+func (r *DeploymentVolumesExist) ID() string {
+	return "DeploymentVolumesExist"
+}
+
+// Desc returns the description of this rule
+func (r *DeploymentVolumesExist) Desc() string {
+	return "All volumes referenced by volumeMounts must exist"
+}
+
+// Exec checks compliance of the given resource with the rule
+func (r *DeploymentVolumesExist) Exec(ctx *context.Context, resource interface{}) (*result.Result, error) {
+	res, ok := resource.(api.Deployment)
+	if !ok {
+		return nil, errors.E(
+			errors.Internal,
+			"Wrong resource type",
+		)
+	}
+
+	result := &result.Result{
+		Pass: true,
+	}
+
+	containers := res.Spec.Template.Spec.Containers
+	if containers == nil || len(containers) == 0 {
+		result.Skip = true
+		result.Have = "no containers"
+	}
+
+	volumes := make(map[string]bool)
+	for _, v := range res.Spec.Template.Spec.Volumes {
+		volumes[v.Name] = true
+	}
+
+	haveVolumeMounts := false
+	for _, c := range containers {
+		for _, v := range c.VolumeMounts {
+			haveVolumeMounts = true
+			if _, exists := volumes[v.Name]; !exists {
+				result.Pass = false
+				result.Have = v.Name
+				return result, nil
+			}
+		}
+	}
+
+	if !haveVolumeMounts {
+		result.Skip = true
+		result.Have = "no volumeMounts"
+	}
+
+	return result, nil
+}
---
+++ internal/lint/rule/deploymentVolumesExist_test.go
@@ -0,0 +1,195 @@
+package rule
+
+import (
+	"fmt"
+	"testing"
+
+	v20190817Pod "github.com/grid-x/ds-api-types/management/2019-08-17/pod"
+	deployments "github.com/grid-x/ds-api-types/management/2019-12-10/deployments"
+
+	"github.com/grid-x/gxctl/internal/lint/context"
+	"github.com/grid-x/gxctl/pkg/api"
+)
+
+func TestDeploymentVolumesExist(t *testing.T) {
+	r := DeploymentVolumesExist{}
+
+	testcases := []struct {
+		desc      string
+		ctx       *context.Context
+		res       interface{}
+		wantPass  bool
+		wantSkip  bool
+		wantError bool
+	}{
+		{
+			desc: "volumes exist",
+			ctx:  nilCtx,
+			res: api.Deployment{
+				Spec: deployments.DeviceDeploymentSpec{
+					App: "foo",
+					Template: deployments.PodTemplate{
+						Spec: v20190817Pod.PodConfig{
+							Volumes: []v20190817Pod.Volume{
+								{
+									Name: "foovolume",
+								},
+								{
+									Name: "goovolume",
+								},
+							},
+							Containers: []v20190817Pod.Container{
+								{
+									VolumeMounts: []v20190817Pod.VolumeMount{
+										{
+											Name:      "foovolume",
+											MountPath: "/etc/foobar",
+										},
+										{
+											Name:      "goovolume",
+											MountPath: "/etc/goobaz",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantPass:  true,
+			wantSkip:  false,
+			wantError: false,
+		},
+		{
+			desc: "volume does not exist",
+			ctx:  nilCtx,
+			res: api.Deployment{
+				Spec: deployments.DeviceDeploymentSpec{
+					App: "foo",
+					Template: deployments.PodTemplate{
+						Spec: v20190817Pod.PodConfig{
+							Volumes: []v20190817Pod.Volume{
+								{
+									Name: "foovolume",
+								},
+							},
+							Containers: []v20190817Pod.Container{
+								{
+									VolumeMounts: []v20190817Pod.VolumeMount{
+										{
+											Name:      "foovolume",
+											MountPath: "/etc/foobar",
+										},
+										{
+											Name:      "goovolume",
+											MountPath: "/etc/goobaz",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantPass:  false,
+			wantSkip:  false,
+			wantError: false,
+		},
+		{
+			desc: "no containers",
+			ctx:  nilCtx,
+			res: api.Deployment{
+				Spec: deployments.DeviceDeploymentSpec{
+					App: "app-foo",
+					Template: deployments.PodTemplate{
+						Spec: v20190817Pod.PodConfig{
+							Containers: []v20190817Pod.Container{},
+						},
+					},
+				},
+			},
+			wantPass:  true,
+			wantSkip:  true,
+			wantError: false,
+		},
+		{
+			desc: "no volumes",
+			ctx:  nilCtx,
+			res: api.Deployment{
+				Spec: deployments.DeviceDeploymentSpec{
+					App: "app-foo",
+					Template: deployments.PodTemplate{
+						Spec: v20190817Pod.PodConfig{
+							Volumes: []v20190817Pod.Volume{},
+							Containers: []v20190817Pod.Container{
+								{
+									VolumeMounts: []v20190817Pod.VolumeMount{
+										{
+											Name:      "foovolume",
+											MountPath: "/etc/foobar",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantPass:  false,
+			wantSkip:  false,
+			wantError: false,
+		},
+		{
+			desc: "no volumeMounts",
+			ctx:  nilCtx,
+			res: api.Deployment{
+				Spec: deployments.DeviceDeploymentSpec{
+					App: "foo",
+					Template: deployments.PodTemplate{
+						Spec: v20190817Pod.PodConfig{
+							Containers: []v20190817Pod.Container{
+								{
+									Name:         "container-without-volumeMounts",
+									VolumeMounts: []v20190817Pod.VolumeMount{},
+								},
+							},
+							Volumes: []v20190817Pod.Volume{
+								{
+									Name: "foovolume",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantPass:  true,
+			wantSkip:  true,
+			wantError: false,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("%s", tc.desc), func(t *testing.T) {
+			got, gotErr := r.Exec(tc.ctx, tc.res)
+			got.SourceID = "test"
+			got.Rule = &r
+
+			didErr := false
+			if tc.wantError && gotErr == nil || !tc.wantError && gotErr != nil {
+				t.Errorf("wanted error=%v, got error=%v", tc.wantError, gotErr)
+				didErr = true
+			}
+			if tc.wantPass != got.Pass {
+				t.Errorf("wanted pass=%v, got pass=%v", tc.wantPass, got.Pass)
+				didErr = true
+			}
+			if tc.wantSkip != got.Skip {
+				t.Errorf("wanted skip=%v, got skip=%v", tc.wantSkip, got.Skip)
+				didErr = true
+			}
+			if didErr {
+				t.Errorf("have: %v", got.Have)
+			}
+		})
+	}
+}
