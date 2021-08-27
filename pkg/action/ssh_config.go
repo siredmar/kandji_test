830419ab677cedb1ed0efc330adf2b77a4e013e6
ecktom <t.eck@gridx.de>
2021-08-27 17:11:06 +0200

    feat(ssh): expire ssh key after 60s to remove it from the agent

--- pkg/action/ssh_config.go
+++ pkg/action/ssh_config.go
@@ -30,7 +30,7 @@ var configWant = map[string]map[string]string{
 		"StrictHostKeyChecking":  "no",
 		"LogLevel":               "ERROR",
 		"PubkeyAcceptedKeyTypes": "+ssh-rsa",
-		"ProxyCommand":           "ssh -o 'ForwardAgent yes' $(echo %n | cut -d'.' -f1).gridbox-tunnel 'ssh-add -q /keys/id_wssh_rsa && nc %h %p'",
+		"ProxyCommand":           "ssh -o 'ForwardAgent yes' $(echo %n | cut -d'.' -f1).gridbox-tunnel 'ssh-add -t 60 -q /keys/id_wssh_rsa && nc %h %p'",
 	},
 }
 
