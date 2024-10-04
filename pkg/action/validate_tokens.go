40d521032a3f9a151718801724993f4716f4fd3a
Aleksei Muratov <a.muratov@gridx.de>
2024-10-04 10:42:12 +0200

    feat(config status): print the current used profile
    - print the current used profile as well
    - improve code readability

--- pkg/action/validate_tokens.go
+++ pkg/action/validate_tokens.go
@@ -6,15 +6,38 @@ import (
 	"github.com/grid-x/gxctl/pkg/service"
 )
 
+const (
+	redGreenClosingTag = "\u001B[39m"
+	redTag             = "\u001B[31m"
+	greenTag           = "\u001B[32m"
+	yellowTag          = "\033[33m"
+	closingYellowTag   = "\033[0m"
+	ok                 = "OK  "
+	fail               = "FAIL"
+	validToken         = "valid token"
+)
+
 func ValidateTokens(s *service.Service) error {
 	for _, p := range s.Client.Auth.Profiles {
 		err := p.Auth.Token.Validate()
 		if err == nil {
-			fmt.Printf("\u001B[32mOK  \u001B[39m Profile %s: \033[32mvalid token\033[0m\n", p.Name)
+			fmt.Printf("%s CurrentProfile: %q Profile %q: %s\n", printGreen(ok), s.Client.Auth.CurrentProfile, p.Name, printYellow(validToken))
 		} else {
-			fmt.Printf("\u001B[31mFAIL\u001B[39m Profile %s: \033[33m%s\033[0m\n", p.Name, err)
+			fmt.Printf("%s CurrentProfile: %q Profile %q: %s\n", printRed(fail), s.Client.Auth.CurrentProfile, p.Name, printYellow(err.Error()))
 		}
 	}
 
 	return nil
+}
+
+func printRed(s string) string {
+	return redTag + s + redGreenClosingTag
+}
+
+func printGreen(s string) string {
+	return greenTag + s + redGreenClosingTag
+}
+
+func printYellow(s string) string {
+	return yellowTag + s + closingYellowTag
 }
