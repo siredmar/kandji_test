199b7b99850c1a08c9c02a8fa5d21bc153340f88
Aleksei Muratov <a.muratov@gridx.de>
2024-12-05 13:48:10 +0100

    feat: capitalize a user facing error

--- pkg/action/logs.go
+++ pkg/action/logs.go
@@ -16,7 +16,7 @@ var (
 	ErrDeviceNotFound       = errors.New("device not found")
 	ErrNotExactSerialNumber = errors.New("there are multiple devices with this serial number pattern. Please give an exact serial number")
 	ErrWrongOwner           = errors.New("there are device logs settings for this device created by another user. If you want to override them, run the command again with the --change-owner flag")
-	ErrInvalidExpiry        = errors.New("logs expiry must not exceed 1 month")
+	ErrInvalidExpiry        = errors.New("Logs expiry must not exceed 1 month")
 	ErrLogsAlreadyExists    = errors.New("Logs are already enabled for this device")
 
 	maxExpiry = time.Hour * 24 * 30 // 1 month
@@ -112,6 +112,7 @@ func EnableLogs(s *service.Service, id string, isSerialNumber bool, logLevel, ou
 	body.Spec.ExpiresAt = api.NewTime(time.Now().Add(expiry))
 	resp, err := s.Client.PostRequest(fmt.Sprintf("%s/%s", api.DeviceLogsEndpoint, id), &body)
 	if err != nil {
+
 		if strings.Contains(err.Error(), "device logs already exists") {
 			return ErrLogsAlreadyExists
 		}
